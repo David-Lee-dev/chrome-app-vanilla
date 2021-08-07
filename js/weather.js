6a3637a245f8f0b338c6bd2cbe13370a3fbe4ea5
Ju Hyeon <aganga7427@gmail.com>
2021-08-07 11:46:44 +0900

    FEAT : Add weather forecast

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,17 @@
-const API_KEY = config("API_KEY");
+const API_KEY = "a36edb034e2afca9af82ce1dafa64b2e";
 
 function onGeoOk(position){
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&unit=matrix`
+  console.log(url)
   fetch(url).then(response => response.json()).then(data => {
-    const weather = document.querySelector("#weather span:first-child")
-    const city = document.querySelector("#weather span:first-child")    
-    const name = data.name;
-    weather.innerText = data.weather[0].main;
+    const state = document.querySelector("#weather #state")
+    const temp = document.querySelector("#weather #temp")
+    const city = document.querySelector("#weather #location")    
+
+    state.innerText = data.weather[0].main;
+    temp.innerText = `${data.main.temp / 10} ℃`
     city.innerText = data.name;
   });
 }
